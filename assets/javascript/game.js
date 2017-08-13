2b5152f3cc9c51cdc44b4921d6f658dd2accc9c3
zazil <zaziltoma@gmail.com>
2017-08-12 22:24:20 -0700

    more changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-<script>
+
 
 // use bootstrap rows/columns for picture and game / underscores
 
@@ -6,17 +6,19 @@
 var babies = ["calf", "fawn", "foal", "hatchling", "joey", "kit", "kitten", "peachick", "piglet", "porcupette", "puggle", "squab", "tadpole", "pup"]
 
 // upon the start of each game, the program will choose a random baby from the babies array
-var baby = computerChoices[Math.floor(Math.random() * babies.length)];
+var baby = babies[Math.floor(Math.random() * babies.length)];
 var noChar = baby.length;
 
+	console.log(baby);
 // starting value each time, variable value gets replaced with each guess
 var remainingGuesses = noChar * 3; 
-
+	
+	console.log(remainingGuesses);
 // Display the underscores for each letter
-var underscores = "_ "
+var underscores = "_ ";
 
-for (i = 1, i < noChar, i++) {
+for (i = 1; i < noChar; i++) {
 	underscores = underscores + "_ "
 }
 
-
+	console.log(underscores)
